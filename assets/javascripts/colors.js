097ce500aa2fc18378baff46283005988429f08b
yazou <yazgazan@gmail.com>
2014-08-19 13:49:44 -0400

    ...

--- assets/javascripts/colors.js
+++ assets/javascripts/colors.js
@@ -10,6 +10,17 @@
     return adjusted;
   }
 
+  var size = function (text) {
+    //Expression to find.
+    var re = /\[size=\s*([a-zA-Z0-9-#]+)]/gi;
+    // Adjust text.
+    var adjusted = text.replace(re, '');
+
+    adjusted = adjusted.replace(/\[\/size\]/gi, "");
+
+    return adjusted;
+  }
+
   var images = function (text) {
     //Expression to find.
     var re = /\[img\](.*?)\[\/img\]/gi;
@@ -35,6 +46,7 @@
       if (text[i].length > 0 && text[i][0] !== "<") {
         text[i] = center(text[i]);
         text[i] = colors(text[i]);
+        text[i] = size(text[i]);
         text[i] = images(text[i]);
       }
     }
